d9408be1866943ef00dfec1581d8b08f9e063910
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-03-18 16:27:10 +0100

    added zookeeper registration

--- rest-ws/src/main/java/org/gbif/vocabulary/restws/Application.java
+++ rest-ws/src/main/java/org/gbif/vocabulary/restws/Application.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -29,6 +30,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @Import(SpringConfig.class)
+@EnableDiscoveryClient
 public class Application {
 
   public static void main(String[] args) {

141ef719a5af8263b2760f933bf31e48da22a542
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-10-07 17:24:58 +0200

    export service

---
+++ model/src/main/java/org/gbif/vocabulary/model/export/VocabularyExport.java
@@ -0,0 +1,48 @@
+package org.gbif.vocabulary.model.export;
+
+import org.gbif.common.shaded.com.fasterxml.jackson.annotation.JsonProperty;
+import org.gbif.vocabulary.model.Concept;
+import org.gbif.vocabulary.model.Vocabulary;
+
+import java.util.List;
+
+/** Models an export of a vocabulary with all its concepts. */
+public class VocabularyExport {
+
+  public static final String METADATA_PROP = "metadata";
+  public static final String VOCABULARY_PROP = "vocabulary";
+  public static final String CONCEPTS_PROP = "concepts";
+
+  @JsonProperty(METADATA_PROP)
+  private ExportMetadata metadata;
+
+  @JsonProperty(VOCABULARY_PROP)
+  private Vocabulary vocabulary;
+
+  @JsonProperty(CONCEPTS_PROP)
+  private List<Concept> concepts;
+
+  public ExportMetadata getMetadata() {
+    return metadata;
+  }
+
+  public void setMetadata(ExportMetadata metadata) {
+    this.metadata = metadata;
+  }
+
+  public Vocabulary getVocabulary() {
+    return vocabulary;
+  }
+
+  public void setVocabulary(Vocabulary vocabulary) {
+    this.vocabulary = vocabulary;
+  }
+
+  public List<Concept> getConcepts() {
+    return concepts;
+  }
+
+  public void setConcepts(List<Concept> concepts) {
+    this.concepts = concepts;
+  }
+}

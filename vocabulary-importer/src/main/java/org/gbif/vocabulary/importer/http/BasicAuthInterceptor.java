121e79e5cc552e6a96a9f2447ed23668308a4089
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-04-30 16:38:41 +0200

    skeleton to import vocabularies

---
+++ vocabulary-importer/src/main/java/org/gbif/vocabulary/importer/http/BasicAuthInterceptor.java
@@ -0,0 +1,27 @@
+package org.gbif.vocabulary.importer.http;
+
+import java.io.IOException;
+
+import okhttp3.*;
+
+/** Interceptor for the {@link OkHttpClient} to add basic auth in all the requests. */
+public class BasicAuthInterceptor implements Interceptor {
+
+  private String credentials;
+
+  public BasicAuthInterceptor(String user, String password) {
+    this.credentials = Credentials.basic(user, password);
+  }
+
+  @Override
+  public Response intercept(Chain chain) throws IOException {
+    Request request = chain.request();
+    Request authenticatedRequest =
+        request
+            .newBuilder()
+            .header("Authorization", credentials)
+            .cacheControl(new CacheControl.Builder().noCache().build())
+            .build();
+    return chain.proceed(authenticatedRequest);
+  }
+}

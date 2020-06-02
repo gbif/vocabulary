a027968909653ee01d668ec56788cdc6d7ccdcdc
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-06-02 16:25:12 +0200

    service and resource to release vocabulary versions (ITs of the resource pending)

--- core/src/main/java/org/gbif/vocabulary/service/config/ExportConfig.java
+++ core/src/main/java/org/gbif/vocabulary/service/config/ExportConfig.java
@@ -1,9 +1,11 @@
 package org.gbif.vocabulary.service.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
+import com.google.common.base.Strings;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +15,36 @@ import lombok.Setter;
 @Setter
 public class ExportConfig {
 
-  @NotBlank private String deployUser;
-  @NotBlank private String deployPassword;
-  @NotBlank private String deployRepository;
-  private boolean deployEnabled;
+  private boolean releaseEnabled;
+  private String deployUser;
+  private String deployPassword;
+  private String deployRepository;
 
+  /** Custom {@link Validator} for {@link ExportConfig}. */
+  public static class ExportConfigValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+      return ExportConfig.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+      ExportConfig exportConfig = (ExportConfig) target;
+
+      if (exportConfig.isReleaseEnabled()) {
+        if (Strings.isNullOrEmpty(exportConfig.getDeployUser())) {
+          errors.rejectValue("deployUser", "Deploy user is required if releases are enabled");
+        }
+        if (Strings.isNullOrEmpty(exportConfig.getDeployPassword())) {
+          errors.rejectValue(
+              "deployPassword", "Deploy password is required if releases are enabled");
+        }
+        if (Strings.isNullOrEmpty(exportConfig.getDeployUser())) {
+          errors.rejectValue(
+              "deployRepository", "Deploy repository is required if releases are enabled");
+        }
+      }
+    }
+  }
 }

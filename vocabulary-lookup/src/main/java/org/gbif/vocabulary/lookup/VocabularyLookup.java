25783e9ac8934723c5b85190fe6bd6edbfc8fdcf
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2021-07-14 10:37:15 +0200

    ignore unknown properties in deserialization in lookup

--- vocabulary-lookup/src/main/java/org/gbif/vocabulary/lookup/VocabularyLookup.java
+++ vocabulary-lookup/src/main/java/org/gbif/vocabulary/lookup/VocabularyLookup.java
@@ -15,11 +15,6 @@
  */
 package org.gbif.vocabulary.lookup;
 
-import org.gbif.vocabulary.model.Concept;
-import org.gbif.vocabulary.model.LanguageRegion;
-import org.gbif.vocabulary.model.export.VocabularyExport;
-import org.gbif.vocabulary.tools.VocabularyDownloader;
-
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -34,14 +29,19 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
+import org.gbif.vocabulary.model.Concept;
+import org.gbif.vocabulary.model.LanguageRegion;
+import org.gbif.vocabulary.model.export.VocabularyExport;
+import org.gbif.vocabulary.tools.VocabularyDownloader;
+
 import org.cache2k.Cache;
 import org.cache2k.Cache2kBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
@@ -93,7 +93,9 @@ public class VocabularyLookup implements AutoCloseable, Serializable {
 
   private static final Logger LOG = LoggerFactory.getLogger(VocabularyLookup.class);
   private static final ObjectMapper OBJECT_MAPPER =
-      new ObjectMapper().registerModule(new JavaTimeModule());
+      new ObjectMapper()
+          .registerModule(new JavaTimeModule())
+          .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
   private final Cache<String, Concept> namesCache;
   private final Cache<String, LabelMatch> labelsCache;

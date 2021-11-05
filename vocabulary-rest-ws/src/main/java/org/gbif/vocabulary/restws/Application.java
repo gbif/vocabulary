042e092f3d9b7ab27484a47d4784e08aaeba580a
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2021-11-05 16:19:02 +0100

    using static factory method to create RemoteAuthClient

--- vocabulary-rest-ws/src/main/java/org/gbif/vocabulary/restws/Application.java
+++ vocabulary-rest-ws/src/main/java/org/gbif/vocabulary/restws/Application.java
@@ -13,6 +13,9 @@
  */
 package org.gbif.vocabulary.restws;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.gbif.common.messaging.ConnectionParameters;
 import org.gbif.common.messaging.DefaultMessagePublisher;
 import org.gbif.common.messaging.api.MessagePublisher;
@@ -28,10 +31,6 @@ import org.gbif.ws.server.filter.HttpServletRequestWrapperFilter;
 import org.gbif.ws.server.filter.RequestHeaderParamUpdateFilter;
 import org.gbif.ws.server.provider.PageableHandlerMethodArgumentResolver;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -45,14 +44,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -82,22 +79,10 @@ public class Application {
   }
 
   @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder builder, SecurityConfig securityConfig) {
-    return builder
-        .setConnectTimeout(Duration.ofSeconds(30))
-        .setReadTimeout(Duration.ofSeconds(60))
-        .rootUri(securityConfig.getLoginApiBasePath())
-        .additionalInterceptors(
-            (request, body, execution) -> {
-              request.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-              return execution.execute(request, body);
-            })
-        .build();
-  }
-
-  @Bean
-  public RemoteAuthClient remoteAuthClient(RestTemplate restTemplate) {
-    return new RestTemplateRemoteAuthClient(restTemplate);
+  public RemoteAuthClient remoteAuthClient(
+      RestTemplateBuilder builder, SecurityConfig securityConfig) {
+    return RestTemplateRemoteAuthClient.createInstance(
+        builder, securityConfig.getLoginApiBasePath());
   }
 
   @Bean

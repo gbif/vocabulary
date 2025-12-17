8f99697c1201d3786105de154c7e5b3763b0f7fe
ahakanzn <a.hakan.zn@gmail.com>
2025-12-17 12:33:27 +0100

    clean tables for tests

--- core/src/test/resources/clean-db.sql
+++ core/src/test/resources/clean-db.sql
@@ -1,3 +1,8 @@
+DELETE FROM concept_definition;
+DELETE FROM concept_label;
+DELETE FROM concept_alternative_label;
+DELETE FROM concept_hidden_label;
 DELETE FROM concept;
+DELETE FROM vocabulary_definition;
 DELETE FROM vocabulary_label;
 DELETE FROM vocabulary;
--- vocabulary-rest-ws/src/test/resources/clean-db.sql
+++ vocabulary-rest-ws/src/test/resources/clean-db.sql
@@ -1,5 +1,7 @@
 DELETE FROM concept_definition;
 DELETE FROM concept_label;
+DELETE FROM concept_alternative_label;
+DELETE FROM concept_hidden_label;
 DELETE FROM concept;
 DELETE FROM vocabulary_definition;
 DELETE FROM vocabulary_label;

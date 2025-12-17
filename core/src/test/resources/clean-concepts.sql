3814fa0df9086f847ba1068b00adc7ddb0806f04
ahakanzn <a.hakan.zn@gmail.com>
2025-12-17 12:47:17 +0100

    clean tables for tests

--- core/src/test/resources/clean-concepts.sql
+++ core/src/test/resources/clean-concepts.sql
@@ -1,3 +1,6 @@
+DELETE FROM concept_tag;
+DELETE FROM tag;
+DELETE FROM concept_definition;
 DELETE FROM concept_label;
 DELETE FROM concept_alternative_label;
 DELETE FROM concept_hidden_label;
--- vocabulary-rest-ws/src/test/resources/clean-db.sql
+++ vocabulary-rest-ws/src/test/resources/clean-db.sql
@@ -1,3 +1,6 @@
+DELETE FROM vocabulary_release;
+DELETE FROM concept_tag;
+DELETE FROM tag;
 DELETE FROM concept_definition;
 DELETE FROM concept_label;
 DELETE FROM concept_alternative_label;
